aa31c49593f544e0417341bc320b996ed583bc37
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-07 13:51:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,10 +19,11 @@ class Student
       DB[:conn].execute(sql)
   end
   
-  def self.drop_table 
-    sql = <<- SQL 
-    DROP TABLE students 
-    SQL 
-    DB[:conn].execute(sql)
-  end 
-end
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
